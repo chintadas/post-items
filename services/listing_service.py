058b4952e98c6e3e99843d69ed0bbff80b8a716d
Sailesh Chintada <sailesh.chintada@gmail.com>
2026-09-23 20:05:09 -0700

    Set variant price to 1 cent less than suggested

--- services/listing_service.py
+++ services/listing_service.py
@@ -59,9 +59,10 @@ async def process_folder_listing(folder_name: str, item_index: int = 1, total_it
     new_product.status = "draft"
     new_product.options = [{"name": "Size"}]
 
+    suggested_price = round(float(data["price"]) - 0.01, 2)
     variant = shopify.Variant(
         {
-            "price": data["price"],
+            "price": f"{suggested_price:.2f}",
             "option1": data["size"],
             "inventory_management": "shopify",
         }
